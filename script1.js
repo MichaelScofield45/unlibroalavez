7a5aa696d5df5b696e471fadee11db1b5d47d7f9
MichaelScofield45 <68405832+MichaelScofield45@users.noreply.github.com>
2020-11-08 15:20:50 -0600

    Added support for basic passwords
    Using the prompt() method, be able to use JavaScript to pull that string and make a cookie in the browser. That way the user only has to "login" once, and when the tab is closed the password is prompted again.

---
+++ script1.js
@@ -0,0 +1,41 @@
+//var password;
+//var pass1 = 'contra';
+//
+//input = prompt("Please enter password.");
+//
+//if (password == pass1) {
+//    alert("Password Correct, proceed.");
+//}
+//else {
+//    window.location = "index.html";
+//}
+//
+//function setCookie(cname, cvalue) {
+//    
+//    document.cookie = cname + '=' + cvalue; 
+//}
+//
+//function getCookie(cname) {
+//    
+//}
+
+var x = document.cookie;
+
+if (x == "key=123") {
+    console.log('nice');
+}
+
+else {
+    var word = prompt("Password:");
+    document.cookie = "key" + "=" + word;
+    x = document.cookie;
+    location.reload();
+}
+
+//    else {
+//        check(value);
+//    }
+
+console.log(x);
+
+
