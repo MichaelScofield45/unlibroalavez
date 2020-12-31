c18c7e4076a1ba9393419fff76335d188a4a552b
MichaelScofield45 <paco.villamonte@gmail.com>
2020-12-31 12:03:23 -0600

    Linked to local git repo.

---
+++ script1.js
@@ -0,0 +1,39 @@
+//var password;
+//var pass1 = 'contra';
+//
+//input = prompt("Please enter password.");
+//
+//if (password == pass1) {
+//    alert("Password Correct, proceed.");
+//}
+//else {
+//    window.location = "index.html";
+//}
+//
+//function setCookie(cname, cvalue) {
+//    
+//    document.cookie = cname + '=' + cvalue; 
+//}
+//
+//function getCookie(cname) {
+//    
+//}
+
+var x = document.cookie;
+if (document.cookie.split(';').some((item) => item.includes('key=123'))) {
+    console.log('The cookie "key" has "123" for value')
+
+}
+
+//if (x == "key=123") {
+//    console.log('nice');
+//}
+
+else {
+    var word = prompt("Password:");
+    document.cookie = "key" + "=" + word;
+    x = document.cookie;
+    location.reload();
+}
+
+
